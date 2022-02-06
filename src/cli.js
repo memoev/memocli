f1f6e9e16e3f64f874f685627fdf08907374bd1a
Guillermo Villalta <mexcelus@gmail.com>
2022-02-05 23:39:58 -0700

    create a repo in github

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,12 @@
 import arg from 'arg';
+import { execSync } from 'child_process';
 
 function parseArgumentsIntoOptions(rawArgs) {
  const args = arg(
    {
      '--help': Boolean,
      '--version': Boolean,
+     '--create': Boolean,
      '--h': '--help',
      '--v': '--version',
    },
@@ -12,19 +14,26 @@ function parseArgumentsIntoOptions(rawArgs) {
      argv: rawArgs.slice(2),
    }
  );
+
+ console.log(args);
+ 
  if (args['--version']) {
    console.log('v1.0.0')
  }
  if (args['--help']) {
   console.log('help tbd')
 }
+if (args['--create']) {
+  if (args._[0] === 'ghrepo') {
+    const output = execSync(`gh repo create ${args._[1]} --public`, { encoding: 'utf-8' });
+    console.log(output)
+  }
+
+}
  return {
-   help: args['--help'] || false,
-   version: args['--version'] || false,
  };
 }
 
 export function cli(args) {
  let options = parseArgumentsIntoOptions(args);
- console.log(options);
 }
